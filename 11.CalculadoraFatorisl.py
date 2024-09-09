9218fcd50964d983cd6b14ea6792110ba91f5527
Hadassa Victória <hadassasouzaviic@gmail.com>
2024-09-09 11:15:40 -0300

    Add files via upload

---
+++ 11.CalculadoraFatorisl.py
@@ -0,0 +1,9 @@
+#Calculadora Fatorial
+
+numero = int(input("Digite um numero:"))
+fatorial = 1
+contador = 1 
+
+while (contador <= numero):
+    fatorial = fatorial * contador
+    contador = contador + 1 
