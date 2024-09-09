44de539e681ea54294742e604bb7b21313d4f7a6
Hadassa Victória <hadassasouzaviic@gmail.com>
2024-09-09 11:04:53 -0300

    Add files via upload

---
+++ 11.Tabuada.py
@@ -0,0 +1,12 @@
+#Tabuada
+
+contador = 0
+numero = int(input("Digite um valor da tabuada"))
+
+while (contador <= 10):
+    resultado = numero * contador
+    print(f"{numero} x {contador} = {resultado}")
+    contador += 1
+    
+
+
